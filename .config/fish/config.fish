24ac66f133a8a35f0e895ce80616f74cf9b30138
Jayanth Parthsarathy <jparthaa@gmail.com>
2023-09-10 18:41:30 +0530

    Add tmux sessionizer

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,10 @@ if status is-interactive
     set -Ux MOZ_ENABLE_WAYLAND 1
     set -Ux GTK_THEME Catppuccin-Mocha-Standard-Mauve-dark
     set -Ux BROWSER firefox
+    set -Ux TERM "TERM=foot-256color"
     alias config "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
+    alias fetch "diyfetch"
+    alias fz "fuzzy_tmux"
     set -Ux PF_INFO "ascii title os host kernel uptime pkgs memory"
     alias docker-set "sudo chmod 666 /var/run/docker.sock"
     alias nvi nvim
