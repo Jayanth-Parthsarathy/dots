20535f88701b303188d43dc7dd4027a7fd1c3521
Jayanth Parthsarathy <jparthaa@gmail.com>
2023-09-14 20:59:58 +0530

    Change to gruvbox and display music

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,20 @@ if status is-interactive
     set fish_greeting
     set -x XDG_CONFIG_HOME "$HOME/.config"
     set -Ux MOZ_ENABLE_WAYLAND 1
-    set -Ux GTK_THEME Catppuccin-Mocha-Standard-Mauve-dark
+    set -Ux GTK_THEME gruvbox-dark-gtk
     set -Ux BROWSER firefox
     set -Ux TERM "TERM=foot-256color"
     alias config "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
     alias fetch "diyfetch"
     alias fz "fuzzy_tmux"
     set -Ux PF_INFO "ascii title os host kernel uptime pkgs memory"
-    alias docker-set "sudo chmod 666 /var/run/docker.sock"
+    alias docker-set "sudo chmod 777 /var/run/docker.sock"
     alias nvi nvim
     alias gc "git clone"
     starship init fish | source
+    theme_gruvbox dark
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
